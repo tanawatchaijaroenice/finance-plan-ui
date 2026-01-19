079068b4bdb6d23aa2f6fadaf9b53742f9174ca5
Tanawayicez <tanawat.chaijaroenice@gmail.com>
2026-01-20 00:47:18 +0700

    fix: api url auth env

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 
-const API_URL = 'http://localhost:3000/api';
+
+const API_URL = 'https://finance-plan-api.onrender.com/api';
+// const API_URL = 'http://localhost:3000/api';
 
 @Injectable({
     providedIn: 'root'
